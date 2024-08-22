016f8e3614e63869ed2cc36ec952d7a961be190f
mpate154 <mpate154@stevens.edu>
2024-08-22 15:46:40 -0400

    added slides icon to writing projects

--- script.js
+++ script.js
@@ -47,3 +47,23 @@ function loop() {
 }
 
 loop();
+
+function showTabContent(event, tabName) {
+  var i, tabContent, tabLinks;
+
+  // Hide all tab content
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  // Remove 'active' class from all tab links
+  tabLinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove("active");
+  }
+
+  // Show the current tab and add 'active' class to the clicked tab
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.classList.add("active");
+}
